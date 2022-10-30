8b12e808fe1326f16c401d210a37e7aa2e751f00
Daniel <danissimojblack@gmail.com>
2022-10-30 22:54:11 +0200

    init view customized

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,17 @@
+import './Categories.scss'
+import CategoryItem from '../category-item/CategoryItem'
+const Categories = (props) => {
+    const {categories} = props
+    
+    const items = categories.map(({id, title, imageUrl})=> {
+        return <CategoryItem key={id} title={title} imageUrl={imageUrl}/>
+      })
+
+    return (
+        <div className="categories-container">
+          {items}
+        </div>
+    )
+}
+ 
+export default Categories;
---
+++ src/components/category-item/CategoryItem.js
@@ -0,0 +1,17 @@
+import './CategoryItem.scss'
+
+const CategoryItem = ({title, imageUrl}) => {
+    return (
+        <div className="category-container">
+          <div className="background-image" style={{
+            'backgroundImage': `url(${imageUrl})`
+          }}/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      )
+}
+ 
+export default CategoryItem;
